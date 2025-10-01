4544141fa61582f3c9f32a5ea5dfd5371b676c37
ayoub hanaf <ahanaf@ahanaf.42.fr>
2025-10-01 11:31:15 +0100

    project done

---
+++ srcs/requirements/bonus/ftp/tools/ftp-entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+set -eu
+
+echo ">>>>Creating empty dir"
+mkdir -p /var/run/vsftpd/empty
+chmod 755 /var/run/vsftpd/empty
+
+echo ">>>>Creating ${FTP_USER}"
+# Alpine adduser syntax
+adduser -D -h /home/${FTP_USER} -s /sbin/nologin ${FTP_USER} 
+echo "${FTP_USER}:${FTP_PASS}" | chpasswd
+
+echo ">>>>Adding ${FTP_USER} to www-data group"
+addgroup ${FTP_USER} www-data || true
+adduser ${FTP_USER} www-data || true
+
+echo "removing write permission for /home/${FTP_USER}"
+chmod a-w /home/${FTP_USER}
+mkdir -p /home/${FTP_USER}/wordpress
+chown -R ${FTP_USER}:${FTP_USER} /home/${FTP_USER}/wordpress
+
+echo ">>>>Starting vsftpd"
+vsftpd /etc/vsftpd/vsftpd.conf
+
